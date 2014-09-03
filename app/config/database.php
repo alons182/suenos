5218c7531ecc4fe61a869cd546223e424d7250c1
alonso <alons182@hotmail.com>
2014-09-03 14:33:58 -0600

    update params database production

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'kuzanagi_suenos',
+			'username'  => 'kuzanagi_alonso',
+			'password'  => 'Iw6eVdVMGcaZ',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
