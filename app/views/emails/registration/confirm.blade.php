1415a2a3a23c1f0bca3e632e9473e536e030f41c
alonso <alons182@hotmail.com>
2014-10-22 18:39:16 -0600

    changed mail confirmation of registration

--- app/views/emails/registration/confirm.blade.php
+++ app/views/emails/registration/confirm.blade.php
@@ -5,8 +5,21 @@
     <title>Bienvenido a Sueños de vida</title>
 </head>
 <body>
-<p>Gracias por registrarte a sueños de vida, puedes ingresar a tu cuenta con el correo <strong>{{ $email }}</strong>  y la contraseña utilizada en el proceso de registro</p>
-<p>Tambien puedes compartir esta url {{ URL::to('/'.$username) }} con otros usuarios para que se agreguen a tu red. </p>
 
+<strong>Url para compartir:</strong> {{ URL::to('/'.$username) }}
+<strong>Su Usuario es:</strong> {{ $username }}
+
+<h1>¡Felicidades ya eres parte de sueños de vida!</h1>
+
+  <p> Recuerda que te inscribiste como afiliado de Sueños de Vida y deberás seguir los siguientes pasos para poder obtener tus ingresos Residuales.</p>
+
+  <p> Realiza el pago de tu membresía si aún no lo haz efectuado. Recuerda que el pago solo se puede efectuar en las cuentas bancarias de la Empresa a nombre Sueños de Vida costa rica S.A, ninguna persona o afiliado está autorizado a realizar ningún cobro bajo ningún concepto.</p>
+
+   <p>Formaliza tu inscripción enviando la documentación que corresponde, recuerda que si no estas Activo durante el plazo preestablecido serás eliminado del Sistema.</p>
+
+   <p>Si deseas patrocinar a alguna persona y sumarla a tu red, tendrás que pasarle tu link personal e indicarle que le de un clic y  complete el formulario con los datos que se le solicitan.</p>p
+
+  <p> Para ingresar a tu sitio personal podrás hacerlo desde el sitio web principal de la empresa o en tu link personal da un  Clic " login" allí deberás colocar tu correo electrónico y Clave para loguearte. En tu sitio personal  Podrás llevar el control de tus regalías y movimientos de tu red de afiliados.</p>
+</p>
 </body>
 </html>
