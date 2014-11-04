77a8db64104997c96a26bcb649a5dbe58f457f0b
Alonso Chavarria <alons182@hotmail.com>
2014-11-04 13:39:39 -0600

    fix about oportuniy text

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -6,55 +6,29 @@
 
     <p><img class="img" src="/img/nosotros.jpg" alt="Trabajo equipo"
             style="float: right; width:350px;margin-left: 10px;"/
-        Sueños de vida nace precisamente como su nombre lo indica “Un sueño” que pretende cambiar la historia de los
-        programas de afiliados, que se enfocan en un sector determinado con programas imposibles de completar o
-        simplemente estafas para aprovecharse de la confianza de las personas.</p>
+        Sueños de vida nace precisamente como su nombre lo indica “Un sueño”, un sueño que fuera el motor y el impulso para muchos otros sueños que se unirían con las personas que decidieran formar parte de esta empresa. Para lograr  estos  se debería cambiar la historia de los programas de afiliados, que se enfocan en un sector determinado, con programas imposibles de completar o simplemente se transformaban en engaños para aprovecharse de la confianza de las personas.</p>
 
-    <p> Esta empresa nace en la cabeza de una persona como cualquiera, que cansado de ver como excelentes propuestas de
-        negocios terminaban convirtiéndose en un sueño imposible de alcanzar ya fuera por el sistema que empleaban o
-        simplemente por la falta de cumplimiento en los pagos por parte de las empresas. Después de muchas desilusiones
-        e intentos fallidos por desarrollar una red de afiliados, toma la decisión de investigar un poco sobre las redes
-        de afiliados y la posibilidad de desarrollar una empresa de marketing de afiliados, qué beneficios podría
-        obtener y qué beneficios podría brindar a las personas que decidieran pertenecer a este sueño que poco a poco se
-        iría convirtiendo en una realidad.</p>
+    <p> Esta empresa nace de la creatividad de un Guanacasteco, que cansado de ver como excelentes propuestas de negocios terminaban convirtiéndose en un sueño imposible de alcanzar, ya fuera por el sistema empleado o simplemente por el incumplimiento de los pagos establecidos, toma la decisión de investigar  sobre las redes de mercadeo y la posibilidad de desarrollar  una empresa de marketing de afiliados ¿ qué beneficios podría brindar? y ¿ qué podría ofrecer a las personas que decidieran pertenecer  a este sueño? que poco a poco se iría convirtiendo en una realidad.</p>
 
-    <p>Con el conocimiento necesario y la visión clara de hacia dónde llegar, inicia el camino con el desarrollo de un
-        sitio web y un programa de control de afiliados fácil de manejar que le permite a las personas desarrollar con
-        facilidad una red de afiliados además de asegurar el control de las ganancias y el conocimiento de sus afiliados
-        activos. De esta manera la empresa logra llevar un seguimiento y dar una correcta distribución de los fondos que
-        las personas nos confían con la firme convicción de que a corto plazo empezarán a dar frutos y se transformaran
-        en un ingreso residual permanente.</p>
+    <p>Con una clara visión y el conocimiento necesario, se inicia el desarrollo de un sitio web y un programa de control de afiliados fácil de manejar, que le permita a las personas desarrollar con facilidad una red de afiliados; además de asegurar el control de las ganancias y  el conocimiento de sus afiliados activos. De esta manera la empresa logrará llevar un seguimiento y dar una correcta distribución de los fondos que las personas nos confían, con la firme convicción de que a corto plazo darán frutos y se transformaran en un ingreso residual permanente.</p>
 
-    <P>Sueños de vida como empresa Costarricense, 100% Guanacasteca inicia operaciones en esta provincia olvidada por el
-        gobierno y desamparada al desarrollo que no sea gracias a su gran belleza y al enorme atractivo que representan
-        sus playas, costumbres, tradiciones, el calor de su gente y su clima ideal para unas vacaciones inolvidables.
-        Como todos los grandes programas están centralizados en la meseta central nuestra empresa decidió darle vuelta a
-        lo tradicional y convertir a Guanacaste en la cuna de esta gran oportunidad de negocios.
+    <P>Sueños de vida como empresa Costarricense, 100% Guanacasteca, inicia operaciones  en esta provincia de gran belleza y enorme atractivo. Ya que la mayoría de los grandes programas están localizados en la meseta central, nuestra empresa decidió dar un giro a lo tradicional y convertir a Guanacaste en la cuna de esta gran oportunidad de negocios, y brindar a los guanacastecos la oportunidad de ser los emprendedores de este programa de mercadeo.
 
     </p>
 
     <h2>Misión sueños de vida</h2>
 
     <p><img class="img" src="/img/trabajo-equipo.jpg" alt="Trabajo equipo"
-            style="float: right; width:350px;margin-left: 10px;"/>Somos una compañía con el firme objetivo de ayudar a
-        construir y apoyar los sueños de muchas personas. Enfocándonos en promover el desarrollo económico mediante un
-        plan de negocios firme y confiable, bajo una solida estructura de trabajo en equipo.
-        Buscamos proponer las mejores opciones de negocio que aumenten el beneficio de compra, ofreciendo a nuestros
-        afiliados la oportunidad de cumplir sus sueños de vida.</p>
+            style="float: right; width:350px;margin-left: 10px;"/>Somos una compañía con el firme objetivo de ayudar a construir y apoyar los sueños de muchas personas. Enfocándonos en promover el desarrollo económico mediante un plan de negocios firme y confiable, bajo una solida estructura de trabajo en equipo. </p>
+        <p>Buscamos proponer las mejores opciones de negocio que aumenten el beneficio de compra, ofreciendo a nuestros afiliados la oportunidad de cumplir sus sueños de vida. </p>
 
     <p>
-        Sueños de vida nace con la consigna de dar un giro positivo a la economía de la región, y provocar un cambio
-        social significativo con proyectos a corto, mediano y largo plazo; Los cuales se tejen como sueños en la memoria
-        de la empresa, que no busca solo el beneficio propio, también pretende dejar una huella positiva donde estemos
-        presente.
+       Sueños de vida nace con la consigna de dar un giro positivo a la economía de la región, y provocar un cambio social significativo con proyectos a corto, mediano y largo plazo; Los cuales se tejen como sueños en la memoria de la empresa, que no busca solo el beneficio propio, también pretende dejar una huella positiva donde estemos presente.
     </p>
 
     <h2>Visión sueños de vida</h2>
 
-    <p><img class="img" src="/img/vision.jpg" alt="Vision" style="float: left;width:250px;margin-right: 10px; "/> Nos
-        visualizamos dueños de su confianza y de su apoyo, ofreciéndole cada día mas y mejores opciones comerciales para
-        su crecimiento personal y socia, con el respaldo de una red estable de afiliados que sean la mayor prueba de la
-        magnifica opción de negocios que representamos.
+    <p><img class="img" src="/img/vision.jpg" alt="Vision" style="float: left;width:250px;margin-right: 10px; "/>Nos visualizamos dueños de su confianza y de su apoyo, ofreciéndole cada día mas y mejores opciones comerciales para su crecimiento personal y socia, con el respaldo de una red estable de afiliados que sean la mayor prueba de la magnifica opción de negocios que representamos.
     </p>
 
     <p></p>
--- app/views/pages/aid_plan.blade.php
+++ app/views/pages/aid_plan.blade.php
@@ -7,15 +7,17 @@
 
     <h2>Misión sueños de vida</h2>
 
-    <p> Sueños de vida, dentro de los beneficios que ofrece  ha creado un plan para aquellas personas que por alguna razón se les dificulte desarrollar su red, y empezar a generar beneficios económicos dentro de la empresa.</p>
+    <p>Sueños de vida, dentro de los beneficios que ofrece  ha creado un plan para aquellas personas que por alguna razón se les dificulte desarrollar su red, y empezar a generar beneficios económicos dentro de la empresa.</p>
 
     <p>
-       Si transcurrido el primer mes no has logrado agregar una sola persona a tu red, explicando el funcionamiento de la empresa y promoviendo los beneficios que esta ofrece, o simplemente se te dificulta expresar adecuadamente la idea de negocio que representa la empresa, entonces debes seguir estos sencillos pasos:
+      Si transcurrido el primer mes no has logrado agregar una sola persona a tu red, explicando el funcionamiento de la empresa y promoviendo los beneficios que esta ofrece, o simplemente se te dificulta expresar adecuadamente la idea de negocio que representa la empresa, entonces debes cumplir estas condiciones y seguir estos sencillos pasos:
     </p>
-
+        <p>Para que el plan de ayuda se pueda aplicar el siguiente mes, la fecha de  pago de la membresía debe ser anterior al 15 del mes en que se realizara la afiliación. </p>
+        <p>Por ejemplo si tu pago se efectuó el 12 de octubre calificas para solicitar el plan de ayuda en noviembre, si tu pago se realizo el 20 de octubre podrías solicitar el plan de ayuda hasta el mes de diciembre ya que tu pago se hizo después del día 15 del mes.</p>
     <h2>Paso # 1</h2>
 
-    <p><b>Pide ayuda:</b>Dentro de los primeros cinco días de tu segundo mes, envía un correo electrónico a <a href="mailto:administracion@suenosdevidacr.com"><b>administracion@suenosdevidacr.com</b></a> explica la estrategia que has seguido para obtener afiliados, indica que no te ha dado resultados, solicita se te ayude a mejorarla y buscar nuevas ideas para que este pueda tener éxito y empezar a crear tu red de afiliados.</p>
+    <p><b>Pide ayuda:</b>Dentro de los primeros cinco días de tu segundo mes, envía un correo electrónico a <a href="mailto:administracion@suenosdevidacr.com"><b>administracion@suenosdevidacr.com</b></a> explica la estrategia que has seguido para obtener afiliados, indica que no te ha dado resultados, solicita se te ayude a mejorarla y buscar nuevas ideas para que este pueda tener éxito y empezar a crear tu red de afiliados.
+    </p>
 
     <h2>Paso # 2</h2>
 
@@ -23,20 +25,25 @@
 
     <h2>Paso # 3</h2>
 
-    <p><b>Poner en marcha el plan:</b> Cuando todo esté debidamente coordinado deberas seguir los pasos acordados con el representante de la empresa y   poner todo  esfuerzo para recibir los frutos del trabajo en equipo.</p>
+    <p><b>Poner en marcha el plan:</b> Cuando todo este debidamente coordinado deberás seguir los pasos acordados con el representante de la empresa y   poner todo tu esfuerzo para recibir los frutos del trabajo en equipo.</p>
 
 
     <h2>Que pasa de acuerdo a los resultados?</h2>
 
     <ul class="ul-letters">
-        <li>Si la empresa consigue que a tu red ingrese por lo menos una persona, tú te debes comprometer a pagar la comisión administrativa de ese mes y la empresa te seguirá brindando apoyo para para que consigas la segunda persona de tu red.
+        <li>Si la empresa consigue que a tu red ingrese por lo menos una persona, tú te debes comprometer a pagar la comisión administrativa del siguiente mes y la empresa te seguirá brindando apoyo para para que consigas la segunda persona de tu red.
         </li>
         <li>Si la empresa ingresa dos o más personas a tu red el compromiso de apoyo individual queda concluido, manteniéndose siempre el apoyo empresarial.
         </li>
         <li>Si la empresa no logra que a tu red se una por lo menos una persona en tu segundo mes, se compromete a darte el tercer mes gratis y continuar apoyándote para desarrollar tu red.
         </li>
-        <li>Si la empresa no logra ayudarte a ingresar a tu red por lo menos una persona después del mes gratis <b>(tercer mes)</b>, tendrás la posibilidad de elegir: Si quieres continuar intentándolo un mes más <b>(cuarto mes)</b>, que la empresa te otorgaría de manera gratuita, o retirarte y pedir tu baja del sistema y de la empresa.
-            Tanto para las personas que decidieron continuar intentándolo (en caso que no tuvieran éxito) como para quienes decidieron retirarse, la empresa se compromete el <b>quinto mes</b>, a efectuar la devolución del <b>100% de la inversión que realizaste por concepto de membrecía.</b>
+        <li>
+        Si la empresa no logra ayudarte a ingresar a tu red por lo menos una persona después del mes gratis  <b>(tercer mes)</b>, tendrás la posibilidad de elegir: Si quieres continuar intentándolo un mes más <b>(cuarto mes)</b>, que la empresa te otorgaría de manera gratuita, o retirarte y pedir tu baja del sistema y de la empresa.
+       Para las personas que solicitan su baja del sistema, deben hacer la solicitud mediante correo electrónico al email: <a href="mailto:administracion@suenosdevidacr.com"><b>administración@suenosdevidacr.com</b></a>.
+
+       Tanto para las personas que decidieron continuar intentándolo (en caso que no tuvieran éxito) como para quienes decidieron retirarse, la empresa se compromete el <b>quinto mes</b>, a efectuar la devolución del <b>100% de la inversión que realizaste por concepto de membrecía.</b>
+
+
 </li>
     </ul>
 
