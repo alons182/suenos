45e18cec0be33b662c15e9eb6fb9cdbb4678e62b
Alonso Chavarria <alons182@hotmail.com>
2014-11-13 10:31:03 -0600

    fix ssesion test

--- app/routes.php
+++ app/routes.php
@@ -11,12 +11,12 @@ App::bind('Suenos\Categories\CategoryRepository', 'Suenos\Categories\DbCategoryR
 App::bind('Suenos\Products\ProductRepository', 'Suenos\Products\DbProductRepository');
 App::bind('Suenos\Photos\PhotoRepository', 'Suenos\Photos\DbPhotoRepository');
 App::bind('Suenos\Orders\OrderRepository', 'Suenos\Orders\DbOrderRepository');
-/*App::after(function($request, $response)
+App::after(function($request, $response)
 {
     $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
     $response->headers->set('Pragma','no-cache');
     $response->headers->set('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
-});*/
+});
 /**
  * Pages
  */
