be967710a58a9194570e7cb8ede497e0262e84c9
Alonso Chavarria <alons182@hotmail.com>
2014-11-27 13:51:38 -0600

    added google plus

--- app/views/layouts/partials/_footer.blade.php
+++ app/views/layouts/partials/_footer.blade.php
@@ -30,7 +30,7 @@
         <div class="redes">
             <a href="https://www.facebook.com/suenosdevidacr" target="_blank" title="Facebook" class="facebook"><i class="icon icon-facebook"></i></a>
             <a href="https://twitter.com/suenosdevidacr" target="_blank" title="Twitter" class="twitter"><i class="icon icon-twitter"></i></a>
-            <a href="#" title="Google Plus" class="google"><i class="icon icon-googleplus"></i></a>
+            <a href="https://plus.google.com/109324544361115734719" rel="publisher" target="_blank" title="Google Plus" class="google"><i class="icon icon-googleplus"></i></a>
         </div>
     </div>
 </footer>
