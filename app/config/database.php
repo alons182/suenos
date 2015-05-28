c58d9401589a5150b791b2884331e1a528217353
Alonso Chavarria <alons182@hotmail.com>
2015-05-28 11:23:59 -0600

    sueno remove bd

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'kuzanagi_suenos',
-			'username'  => 'kuzanagi_alonso',
-			'password'  => 'Iw6eVdVMGcaZ',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
